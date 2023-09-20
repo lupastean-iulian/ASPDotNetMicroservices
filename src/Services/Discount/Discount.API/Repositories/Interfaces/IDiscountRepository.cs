f72b16066c03974e5b61aa50b0eda60156cbe511
Lupastean Iulian - Gabriel <61457294+lupastean-iulian@users.noreply.github.com>
2023-09-20 12:03:48 +0300

    Add discount api and grpc microservices

---
+++ src/Services/Discount/Discount.API/Repositories/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.API.Entities;
+
+namespace Discount.API.Repositories.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscountAsync(string productName);
+        Task<bool> CreateDiscountAsync(Coupon discount);
+        Task<bool> UpdateDiscountAsync(Coupon discount);
+        Task<bool> DeleteDiscountAsync(string productName);
+
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Repositories/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Grpc.Entities;
+
+namespace Discount.Grpc.Repositories.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscountAsync(string productName);
+        Task<bool> CreateDiscountAsync(Coupon discount);
+        Task<bool> UpdateDiscountAsync(Coupon discount);
+        Task<bool> DeleteDiscountAsync(string productName);
+
+    }
+}
